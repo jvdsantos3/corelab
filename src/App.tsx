9337ce97b5bec776fb618c2e6ec27536767b2587
jvdsantos3 <jv.dossantos012@gmail.com>
2024-08-11 14:59:21 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>O melhor</div>
+    </ThemeProvider>
+  )
+}
+
